701fee985efd463d035361591279c572d0f5b488
snackibara <snackibara@gmail.com>
2023-12-02 00:08:53 +0900

    added text, changed css

--- pages/about.js
+++ pages/about.js
@@ -10,7 +10,10 @@ export default function Profile() {
       <h1>About</h1>
       <h2>作った目的</h2>
       <p>このリポジトリの作成目的は…<br />
+      next.jsの学習です。
       </p>
+      <p>まずはnext.jsのチュートリアル（<a href="https://nextjs.org/learn-pages-router/basics/create-nextjs-app">ここ</a>）を一通りやってみて、その後レイアウトを変更したりページを足したりして瑛ます。</p>
+      <p>なので、元々のファイルにあったものが残っているとか、表示や動きの確認のためにテキストやファイルを足しているので未完成で内容はテキトーです。</p>
     </Layout>
   );
 }
